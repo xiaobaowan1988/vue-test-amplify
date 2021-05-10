5a0b2caf723d09e2fcc0822c5579cce87c3031f4
baoliangwan1988 <baoliangwan1988@gmail.com>
2021-05-10 18:52:01 +0800

    add api

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,67 @@
 <template>
  <amplify-authenticator>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App v2"/>
+    <h1>Note App</h1>
+    <input type="text" v-model="name" placeholder="Note name">
+    <input type="text" v-model="description" placeholder="Note description">
+    <button v-on:click="createNote">Create Note</button>
+    <div v-for="item in notes" :key="item.id">
+      <h3>{{ item.name }}</h3>
+      <p>{{ item.description }}</p>
+    </div>
   </div>
   <amplify-sign-out></amplify-sign-out>
  </amplify-authenticator>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { API } from 'aws-amplify';
+import { createNote } from './graphql/mutations';
+import { listNotes } from './graphql/queries';
+import { onCreateNote } from './graphql/subscriptions';
 
 export default {
   name: 'App',
+  async created() {
+    this.getNotes();
+  },
+  data(){
+    return {
+      name: '',
+      description: '',
+      notes: []
+    }
+  },
   components: {
-    HelloWorld
+  },
+  methods: {
+    async createNote() {
+      const { name, description } = this;
+      if (!name || !description) return;
+      const note = { name, description };
+      await API.graphql({
+        query: createNote,
+        variables: {input: note},
+      });
+      this.name = '';
+      this.description = '';
+    },
+    async getNotes() {
+      const notes = await API.graphql({
+        query: listNotes
+      });
+      this.notes = notes.data.listNotes.items;
+    },
+    subscribe() {
+      API.graphql({ query: onCreateNote })
+        .subscribe({
+          next: (eventData) => {
+            let note = eventData.value.data.onCreateNote;
+            if (this.notes.some(item => item.name === note.name)) return; // remove duplications
+            this.notes = [...this.notes, note];
+          }
+        });
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
-import Amplify from 'aws-amplify';
-import aws_exports from './aws-exports';
-import {
-  applyPolyfills,
-  defineCustomElements,
-} from '@aws-amplify/ui-components/loader';
+import Vue from "vue";
+import App from "./App.vue";
+import "@aws-amplify/ui-vue";
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
 
-Amplify.configure(aws_exports);
-applyPolyfills().then(() => {
-  defineCustomElements(window);
-});
+Amplify.configure(awsconfig);
 
 Vue.config.productionTip = false
 
